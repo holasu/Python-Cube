70946895b6c7c8d418d1213e878742248e8f0495
Sudheera Wijedasa <hola.sudheera@gmail.com>
2023-12-25 17:34:09 +0530

    Dictionary data behaviors

---
+++ dictdemo.py
@@ -0,0 +1,10 @@
+# key and values pairs
+myfriends = {'maleen': 'kaluthara', 'bashi': 'mathara', 'danuka': 'rarhnapura'}
+
+print(type(myfriends))
+print(len(myfriends))
+print(myfriends)
+print(myfriends['maleen'])
+print(str(myfriends.keys()))
+print(myfriends.values())
+
