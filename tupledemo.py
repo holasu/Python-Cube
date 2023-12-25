c32ad159dbdd24787d571a18be6036f587fed100
Sudheera Wijedasa <hola.sudheera@gmail.com>
2023-12-25 16:16:10 +0530

    tuples behaviors

---
+++ tupledemo.py
@@ -0,0 +1,17 @@
+
+mytuple = ("wed")
+print(mytuple)
+print(type(mytuple))
+
+# add , to end of tuple
+mytuple = ("wed",)
+print(mytuple)
+print(type(mytuple))
+var = ("Geeks", "for", "Geeks")
+print(var)
+print(var[1])
+# tuple concat
+print(var + mytuple)
+# tuple multiply
+tuple2 = ("cat", "dog")*2
+print(tuple2)
