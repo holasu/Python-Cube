0e451bf4499e233be934935e7c7d0d489c24a406
Sudheera Wijedasa <hola.sudheera@gmail.com>
2023-12-27 01:27:36 +0530

    Bitwise operators

---
+++ bitwiseopdemo.py
@@ -0,0 +1,13 @@
+number01 = int(input("Enter a number 01 "))
+number02 = int(input("Enter a number 01 "))
+
+print(number01 & number02)
+print(number01 | number02)
+print(number01 ^ number02)
+print(~number01)
+print(number01 << 2)
+print(number02 >> 2)
+
+
+
+
