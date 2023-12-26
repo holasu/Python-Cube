36b8669e207800167ac934d80562d3d185f9579a
Sudheera Wijedasa <hola.sudheera@gmail.com>
2023-12-27 01:28:28 +0530

    Number system

---
+++ numsysdemo.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a Number "))
+numbin = bin(int(input("Enter a Number ")))
+print(num)
+print(type(num))
+print(type(numbin))
+print(numbin)
+print(int(numbin, 2))
+print(bin(num))
+print(oct(num))
+print(hex(num))
+
+
